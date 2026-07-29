50e618ba3e4de872825776062ac8e022c8fe06ab
Victor Tomazoni Renaud <153289221+VitoVito25@users.noreply.github.com>
2026-07-29 13:22:17 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -37,7 +37,4 @@ This module is proprietary software and is licensed under OPL-1.
         ],
     },
     "installable": True,
-    "license": "LGPL-3",
-    "currency": "USD",
-    "price": "0.00"
 }
